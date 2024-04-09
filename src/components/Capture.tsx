75ea988a0066a0ab63cd2312e7c9835eecf36357
eamarquezh <supericman@outlook.com>
2024-04-08 18:41:48 -0600

    update

--- src/components/Capture.tsx
+++ src/components/Capture.tsx
@@ -1,24 +1,36 @@
 export const capture = () => {
-    navigator.mediaDevices.getDisplayMedia({ video: { frameRate: { ideal: 30 } } })
+    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
         .then(media => {
-            const mediaRecorder = new MediaRecorder(media, {
-                mimeType: 'video/webm;codecs=vp8,opus'
-            });
-            mediaRecorder.start()
+            navigator.mediaDevices.getDisplayMedia({ video: { frameRate: { ideal: 30 } } })
+                .then(screenMedia => {
+                    const [screenVideo] = screenMedia.getVideoTracks();
+                    const [cameraVideo, audio] = media.getTracks();
 
-            const [video] = media.getVideoTracks();
-            video.addEventListener("ended", () => {
-                mediaRecorder.stop()
-            });
+                    const mediaRecorder = new MediaRecorder(screenMedia, {
+                        mimeType: 'video/webm;codecs=vp8,opus'
+                    });
 
-            mediaRecorder.addEventListener("dataavailable", (e) => {
-                const link = document.createElement("a")
-                link.href = URL.createObjectURL(e.data)
-                link.download = "captura.webm"
-                link.click();
-            });
+                    mediaRecorder.start()
+
+                    screenVideo.addEventListener("ended", () => {
+                        mediaRecorder.stop()
+                        screenVideo.stop();
+                        cameraVideo.stop();
+                        audio.stop();
+                    });
+
+                    mediaRecorder.addEventListener("dataavailable", (e) => {
+                        const link = document.createElement("a")
+                        link.href = URL.createObjectURL(e.data)
+                        link.download = "captura.webm"
+                        link.click();
+                    });
+                })
+                .catch(error => {
+                    alert('Error al capturar pantalla:'+error);
+                });
         })
         .catch(error => {
-            alert('Error al capturar pantalla:'+error);
+            alert('Error al capturar cámara y audio:'+error);
         });
 }
